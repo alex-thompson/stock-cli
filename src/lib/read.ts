b4fdaf5d54c74cf411496190a0076c2a1f2a6a3a
Alex Thompson <athmpsn@gmail.com>
2022-04-18 13:42:16 -0400

    Clean up

--- src/lib/read.ts
+++ src/lib/read.ts
@@ -1,6 +1,9 @@
 import fs from 'fs'
 import Papa from 'papaparse'
 
+// TODO: There'd need to be more guards and robust error handling here
+// since the files are our entry point for user input. PapaParse is great,
+// but I'd want to put it through its paces with a lot of different test cases
 export const read = async (file: string, header = false): Promise<unknown[]> => {
   return new Promise((resolve, reject) => {
     const readable = fs.createReadStream(file)
